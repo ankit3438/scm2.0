90a7944a35daa8b01e37d5af65344273185b752b
ankit3438 <68580225+ankit3438@users.noreply.github.com>
2024-09-11 15:57:00 +0530

    how you can send the value to the html page by using model attribute and use by th:replace:*{}

--- src/main/java/com/scm/scm20/controllers/PageController.java
+++ src/main/java/com/scm/scm20/controllers/PageController.java
@@ -1,7 +1,14 @@
 package com.scm.scm20.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.scm.scm20.forms.UserForm;
+
+import ch.qos.logback.core.model.Model;
 
 
 @Controller
@@ -26,9 +33,25 @@ public class PageController {
     public String login() {
         return "login";
     }
+
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        UserForm userForm= new UserForm();
+        userForm.setName("ankit jha");
+        model.addAttribute("userForm", userForm);
         return "register";
+    }
+
+    @PostMapping("/do-register")
+    public String processRegister(){
+        //fetch form date
+        //User form me store karwayenge
+        //validate karenge
+        //save to database
+        //message ="registration successfull"
+        
+        //redirect to register page
+        return "redirect:/register";
     }
     
 }
